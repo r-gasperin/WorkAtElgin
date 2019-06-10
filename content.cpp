dbf63e5e63b3d91650017edabf75db5d78b8de91
Rodrigo Angelo Gasperin <rodrigo.gasperin@elgin.com.br>
2019-06-10 19:23:03 +0000

    add code in files types: .java,.cpp,.h remove code in files types: .java,.cpp,.h

--- content.cpp
+++ content.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 
 int main() {
-        cout << "kamqf";
+        cout << "hbyco";
 return 0;
 }
--- content.java
+++ content.java
@@ -1,5 +1,5 @@
-public class Csdutx {
+public class Cgqcrh {
     public static void main() {
-        System.out.println("puceb");
+        System.out.println("wmnfv");
     }
 }
