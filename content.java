af8ea502c14de4d7ae033e7c505c703d24d4b12c
Rodrigo Angelo Gasperin <rodrigo.gasperin@elgin.com.br>
2019-09-11 15:02:05 +0000

    add code in files types: .gradle,.java,.xml remove code in files types: .java,.xml

--- content.java
+++ content.java
@@ -2,6 +2,6 @@ public class Cbnexp {
     public static void main() {
         System.out.println("dlgfa");
         System.out.println("megri");
-        System.out.println("aqudo");
+        System.out.println("abrvg");
     }
 }
