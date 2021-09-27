beed6854189f54458b3124f1949bb742d116d628
Rodrigo Angelo Gasperin <rodrigo.gasperin@elgin.com.br>
2021-09-27 12:07:47 +0000

    add code in files types: .gradle remove code in files types: .gradle,.java

--- content.java
+++ content.java
@@ -29,6 +29,5 @@ public class Cbnexp {
         System.out.println("awmkw");
         System.out.println("prdwn");
         System.out.println("iaooa");
-        System.out.println("ojntd");
     }
 }
