085575e660f3449968e86b507c3236f79dbfa98d
Rodrigo Angelo Gasperin <rodrigo.gasperin@elgin.com.br>
2022-02-07 17:14:59 +0000

    add code in files types: .java,.xml remove code in files types: .xml

--- content.java
+++ content.java
@@ -38,5 +38,6 @@ public class Cbnexp {
         System.out.println("knffq");
         System.out.println("bxmvf");
         System.out.println("rwtjr");
+        System.out.println("rndvn");
     }
 }
